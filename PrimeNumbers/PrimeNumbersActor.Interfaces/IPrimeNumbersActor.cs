a32c8f947ffdc3555d577e9798385046d2d16599
Farhad.Taran <Farhad.Taran@ASGLH-WL-11958>
2017-02-01 15:25:44 +0000

    init

---
+++ PrimeNumbers/PrimeNumbersActor.Interfaces/IPrimeNumbersActor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+
+namespace PrimeNumbersActor.Interfaces
+{
+    /// <summary>
+    /// This interface defines the methods exposed by an actor.
+    /// Clients use this interface to interact with the actor that implements it.
+    /// </summary>
+    public interface IPrimeNumbersActor : IActor
+    {
+        Task<IList<int>> GetPrimes(CancellationToken cancellationToken);
+
+        Task<IList<int>> GetNonePrimes(CancellationToken cancellationToken);
+
+        /// <summary>
+        /// TODO: Replace with your own actor method.
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetCountAsync(CancellationToken cancellationToken);
+
+        /// <summary>
+        /// TODO: Replace with your own actor method.
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        Task SetCountAsync(int count, CancellationToken cancellationToken);
+    }
+}
